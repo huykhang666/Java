915c6106957a7b5f2f7d05563aa929f25a247067
Huy Khang <huykhang666@gmail.com>
2025-11-16 20:28:16 +0700

    lab 09

---
+++ BaiTapLab5-12/src/Lab09/BaiTapLab09.java
@@ -0,0 +1,25 @@
+package Lab09;
+
+import java.util.Scanner;
+
+public class BaiTapLab09 {
+	public static void nhapMasv() {
+		System.out.println("Nhập mã sinh viên: ");
+		while (true) {
+			try {
+
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Run");
+
+		try (Scanner scanner = new Scanner(System.in)) {
+
+		}
+	}
+}
